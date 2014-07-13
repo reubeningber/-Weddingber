05a225f79d8f7c7120b5186922a8784ea79d32ac
Reuben Ingber <reuben.ingber@expandtheroom.com>
2014-07-12 23:08:42 -0400

    Various CSS fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	$('.js-fit-text').fitText();
+	
 	$(document).on('click', '.back-to-top', function(e) {
 		e.preventDefault();
 
 		var topNav = $('.site-nav').offset().top;
-		
+
 		$("html, body").animate({ scrollTop: topNav }, "slow");
 	});
 });
