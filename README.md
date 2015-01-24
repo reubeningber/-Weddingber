27b46d5c2407a62072884f0038db09456af5b580
Reuben Ingber <reuben.ingber@expandtheroom.com>
2015-01-24 10:59:15 -0500

    Completely redid the site cause I hated the old one.

--- README.md
+++ README.md
@@ -1,8 +1,20 @@
-Weddingber
-===========
+# harpsichorcd
+A less boilerplate for [Harp.js](http://harpjs.com/) 
 
-Wedding Website 
+### What is this repository for? ###
 
-October 11 2015
+* Provide a working base install with folder structure, base assets, and boilerplate Gruntfile. 
 
-Atlanta, GA
+#### Base assets include: ####
+* lesshat & clearless
+
+### How do I get set up? ###
+
+[Install Harp.js first](http://harpjs.com/docs/quick-start) 
+
+```sh
+harp init my-site --boilerplate reubeningber/harpsichord
+Run npm install in the root folder to set up Grunt.
+harp server my-site
+```
+
---
+++ www/assets/js/scripts.js
@@ -0,0 +1,85 @@
+$(document).ready(function() {
+	$('#menu').slicknav({
+		label: ''
+	});
+	
+	$('.js-fit-text').fitText();
+	
+	$('body').fitVids();
+
+	$('#clock').countdown('2015/10/11').on('update.countdown', function(event) {
+		var $this = $(this).html(event.strftime(''
+	    + '<span class="digits">%-D</span> Day%!d '
+	    + '<span class="digits">%H</span> Hours '
+	    + '<span class="digits">%M</span> Minutes '
+	    + '<span class="digits">%S</span> Seconds'));
+	});
+
+	$(function() {
+    
+      var atlanta = new google.maps.LatLng(33.7550, -84.3900),
+          pointToMoveTo, 
+          first = true,
+          curMarker = new google.maps.Marker({}),
+          $el;
+      
+      var myOptions = {
+          zoom: 15,
+          center: atlanta,
+          mapTypeId: google.maps.MapTypeId.ROADMAP
+        };
+      
+      var map = new google.maps.Map($("#map_canvas")[0], myOptions);
+    
+      $("#locations li").mouseenter(function() {
+      
+        $el = $(this);
+                
+        if (!$el.hasClass("hover")) {
+        
+          $("#locations li").removeClass("hover");
+          $el.addClass("hover");
+        
+          if (!first) { 
+            
+            // Clear current marker
+            curMarker.setMap(); 
+            
+            // Set zoom back to Chicago level
+            // map.setZoom(10);
+          }
+          
+          // Move (pan) map to new location
+          pointToMoveTo = new google.maps.LatLng($el.attr("data-geo-lat"), $el.attr("data-geo-long"));
+          map.panTo(pointToMoveTo);
+          
+          // Add new marker
+          curMarker = new google.maps.Marker({
+              position: pointToMoveTo,
+              map: map,
+              icon: "../assets/img/marker.png"
+          });
+          
+          // On click, zoom map
+          google.maps.event.addListener(curMarker, 'click', function() {
+             map.setZoom(20);
+          });
+          
+          // Fill more info area
+          $("#more-info")
+            .find("h2")
+              .html($el.find("h3").html())
+              .end()
+            .find("p")
+              .html($el.find(".longdesc").html());
+          
+          // No longer the first time through (re: marker clearing)        
+          first = false; 
+        }
+        
+      });
+      
+      $("#locations li:first").trigger("mouseenter");
+      
+    });
+});
